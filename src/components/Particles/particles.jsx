2c5b472e288abb549046008a06ab6adf289ec576
Franco Pari <francopari@Francos-MacBook-Pro.local>
2022-09-15 15:15:11 -0400

    finalizing

---
+++ src/components/Particles/particles.jsx
@@ -0,0 +1,82 @@
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const Particle = () => {
+  const optionsVar = {
+    fpsLimit: 120,
+    interactivity: {
+      events: {
+        resize: true,
+      },
+      modes: {
+        push: {
+          quantity: 4,
+        },
+        repulse: {
+          distance: 200,
+          duration: 0.4,
+        },
+      },
+    },
+    particles: {
+      color: {
+        value: "#ffffff",
+      },
+      links: {
+        color: "#ffffff",
+        distance: 140,
+        enable: true,
+        opacity: 0.3,
+        width: 1,
+      },
+      move: {
+        directions: "none",
+        enable: true,
+        outModes: {
+          default: "bounce",
+        },
+        random: false,
+        speed: 2,
+        straight: false,
+      },
+      number: {
+        density: {
+          enable: true,
+          area: 800,
+        },
+        value: 60,
+      },
+      opacity: {
+        value: 0.3,
+      },
+      shape: {
+        type: "circle",
+      },
+      size: {
+        value: { min: 1, max: 3 },
+      },
+    },
+    detectRetina: true,
+  };
+
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    await loadFull(main);
+  };
+
+  const particlesLoaded = (container) => {
+    console.log(container);
+  };
+
+  return (
+    <Particles
+      id="tsparticles"
+      init={particlesInit}
+      loaded={particlesLoaded}
+      options={optionsVar}
+    />
+  );
+};
+
+export default Particle;
